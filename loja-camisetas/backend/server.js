8c73efd2af01fa1a928eb049cb9f2d372ada4a64
garletmateus <garletmateus@gmail.com>
2026-02-24 19:24:33 -0300

    primeiro

---
+++ loja-camisetas/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import produtosRoutes from "./routes/produtos.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/produtos", produtosRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
