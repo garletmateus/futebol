2b87818ae603c7a4f9e86cd5d48a04cf09851dfa
garletmateus <garletmateus@gmail.com>
2026-08-11 21:44:58 -0300

    backend e conexao

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,24 @@
 ﻿const { Pool } = require("pg");
-require("dotenv").config();
+const path = require("path");
+
+require("dotenv").config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+console.log("HOST:", process.env.DB_HOST);
+console.log("PORT:", process.env.DB_PORT);
+console.log("DATABASE:", process.env.DB_NAME);
+console.log("USER:", process.env.DB_USER);
+console.log("PASSWORD:", process.env.DB_PASSWORD ? "OK" : "ERRO");
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 6543,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    
-    ssl: {
-        rejectUnauthorized: false
-    }
+    password: process.env.DB_PASSWORD,
+    ssl: true,
+    connectionTimeoutMillis: 30000
 });
 
 module.exports = pool;
